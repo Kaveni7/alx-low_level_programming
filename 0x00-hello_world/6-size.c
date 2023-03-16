f8f89bf7359cc50e0ce25ca2ae0e309238ef96a4
Kaveni7 <kavenifelistas96@gmail.com>
2023-03-16 09:15:11 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,6 @@ printf("Size of int: %zu byte(s)\n", sizeof(int));
 printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of float: %zu byte(s)\n", sizeof(float));
-Return(0);
+
+return(0);
 }
